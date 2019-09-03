5ff86a6847525e43578e7bd305e4120f904ee2bf
Michael Mosmann <michael@mosmann.de>
2019-09-03 16:00:03 +0200

    init project

---
+++ vaadin14/vaadin14-invalid-combobox/src/main/java/de/mosmann/vaadinbugs/spring/MessageBean.java
@@ -0,0 +1,12 @@
+package de.mosmann.vaadinbugs.spring;
+
+import java.time.LocalTime;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageBean {
+
+    public String getMessage() {
+        return "Button was clicked at " + LocalTime.now();
+    }
+}
